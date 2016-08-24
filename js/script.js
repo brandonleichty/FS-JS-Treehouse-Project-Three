b6deb60482c434adcc9f4ff58c1472258c6ae3bc
brandonleichty <brandonl@me.com>
2016-08-23 20:10:56 -0500

    Updated shirt selection
    T-Shirt Info section of the form. For the T-Shirt color menu, only
    display the options that match the design selected in the "Design" menu.
    If the user selects "Theme - JS Puns" then the color menu should only
    display "Cornflower Blue," "Dark Slate Grey," and "Gold."
    If the user selects "Theme - I ♥ JS" then the color menu should only
    display "Tomato," "Steel Blue," and "Dim Grey."
    
    Currently the shirts are set to be “disabled.” Need to remove them so
    they’re not visible.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
 /* Global Variables -------------------------------- */
-var otherField = '<div id="otherFieldDiv"><input type="text" id="other-title" placeholder="Your Title"></div>';
+var otherField = '<input type="text" id="other-title" placeholder="Your Title">';
 
 
 /* DOM append -------------------------------- */
 $("#title").after(otherField);
 
-$("#otherFieldDiv").hide();
+$("#other-title").hide();
 
 //shows the "Your Title" text entry field when "other" is selected
 $("#title").change(function() {
   if ($("#title").val() === "other") {
-      $("#otherFieldDiv").show();
-    } else {
-      $("#otherFieldDiv").hide();
+      $("#other-title").show();
+     } else {
+      $("#other-title").hide();
+   }
+});
+
+
+$("#design").on("change", function() {
+
+  if ($("#design").val() === "js puns") {
+      $("#color > option").siblings().prop( "disabled", false);
+      $("#color > option:nth-of-type(n + 4)").prop( "disabled", true );
+  } else if ($("#design").val() === "heart js") {
+      $("#color > option").siblings().prop( "disabled", false);
+      $("#color > option:nth-of-type(-n + 3)").prop( "disabled", true );
+  } else {
+      $("#color > option").siblings().prop( "disabled", false);
   }
 });
